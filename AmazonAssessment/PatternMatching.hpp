70d589c6ed2e593391bbce68bd6932f5e3537829
Elangbam <jelangba@MGC000000727>
2017-09-01 12:21:35 -0400

    Add new files

---
+++ AmazonAssessment/PatternMatching.hpp
@@ -0,0 +1,23 @@
+//
+//  PatternMatching.hpp
+//  AmazonAssessment
+//
+//  Created by Elangbam, Johnson (J.) on 9/1/17.
+//  Copyright © 2017 Elangbam, Johnson (J.). All rights reserved.
+//
+
+#ifndef PatternMatching_hpp
+#define PatternMatching_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int matchPattern(vector<std::string> str, vector<string> pattern);
+int match2DVectorPattern(std::vector<vector<string>> patternVector, vector<string> cartVector);
+
+
+
+#endif /* PatternMatching_hpp */
--- AmazonAssessment/main.cpp
+++ AmazonAssessment/main.cpp
@@ -9,13 +9,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "PatternMatching.hpp"
 //#include <algorithm>
 
 
 using namespace std;
 
-int match(vector<string> str, vector<string> pattern);
-int match(vector<vector<string>> patternVector, vector<string> cartVector);
 
 
 int main(int argc, const char * argv[]) {
@@ -24,7 +23,7 @@ int main(int argc, const char * argv[]) {
     vector<string> str;
     vector<string> pattern;
     
-    str = {"kiwi","pink","kiwi"};
+    str = {"kiwi","apple","kiwi","orange"};
     
     //pattern = {"mango","anything","apple"};
     
@@ -36,117 +35,16 @@ int main(int argc, const char * argv[]) {
         {"brown","pink"}};
     
     
-    int isFound = match(patternVector, str);
+    int isFound = match2DVectorPattern(patternVector, str);
     
     if (isFound) {
-        cout<<"Pattern found\n";
+        cout<<"Bingo!!\n";
     }else
-        cout<<"Pattern not found\n";
+        cout<<"Good luck next time\n";
+    
     
     return 0;
 }
 
-int match(vector<vector<string>> patternVector, vector<string> cartVector){
-    
-    
-    int i;
-    int j = 0;
-    int isFound = 0;
-    int maxCompare;
-
-    string cartString;
-    string patternString;
-
-    for (int k = 0 ; k < patternVector.size(); k++) {
-        if (isFound == 1) {
-            break;
-        }
-        
-        maxCompare = int(cartVector.size() - patternVector[k].size());
-        i = 0;
-        cartString = cartVector[i];
-        patternString = patternVector[k][j];
-        
-        if (cartVector.size() < patternVector[k].size()) {
-            continue;
-        }
-        
-        while (i <= maxCompare) {
-            
-            while (cartString == patternString || patternString == "anything") {
-                isFound = 1;
-                i++;
-                j++;
-                cartString = cartVector[i];
-                patternString = patternVector[k][j];
-                
-                if (cartString != patternString && patternString != "anything") {
-                    isFound = 0;
-                }
-                
-                if (i  == cartVector.size()-1 || j == patternVector[k].size()-1)
-                    break;
-                
-            }
-            
-            if (j == (int)patternVector[k].size() - 1){
-                break;
-            }
-            isFound = 0;
-            i++;
-            cartString = cartVector[i];
-            
-        }
-        
-    }
-    
-    return isFound;
-
-}
 
 
-//int match(vector<string> str, vector<string> pattern) {
-//    
-//    int position = 0;
-//    int i = 0;
-//    int j = 0;
-//    int isFound = 0;
-//    int counter = int(str.size() - pattern.size());
-//    
-//    
-//    string s = str[i];
-//    string p = pattern[j];
-//    
-//    if (str.size() < pattern.size()) {
-//        return 0;
-//    }
-//    
-//
-//    while (i <= counter) {
-//        
-//        while (s == p || p == "anything") {
-//            isFound = 1;
-//            i++;
-//            j++;
-//            s = str[i];
-//            p = pattern[j];
-//            if (s == "" || p == "")
-//                break;
-//            
-//        }
-//        
-//        if (p == ""){
-//            break;
-//        }
-//        isFound = 0;
-//        i++;
-//        s = str[i];
-//        position++;
-//        
-//    }
-//    
-//    return isFound;
-//    
-//    
-//}
-//
